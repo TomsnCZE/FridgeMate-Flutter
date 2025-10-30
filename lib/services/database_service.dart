4630fe4b66bf6b6fc62bb91c7e60786e601e62a3
tomasmucha <tomikmucha92@gmail.com>
2025-10-30 13:20:43 +0100

    pridan package na SQFlite,

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,31 +1,22 @@
-import 'package:hive/hive.dart';
-import '../models/product.dart';
+import 'package:sqflite/sqflite.dart';
 
-class DatabaseService {
-  static const String boxName = 'productsBox';
-
-  Future<void> addProduct(Product product) async {
-    final box = await Hive.openBox(boxName);
-    await box.add(product.toJson());
-    print('✅ Produkt přidán: ${product.name}'); // DEBUG
-  }
-
-  Future<List<Product>> getProducts() async {
-    final box = await Hive.openBox(boxName);
-    final products = box.values
-        .map((e) => Product.fromJson(Map<String, dynamic>.from(e)))
-        .toList();
-    print('✅ Načteno produktů: ${products.length}'); // DEBUG
-    return products;
-  }
-
-  Future<void> deleteProduct(int index) async {
-    final box = await Hive.openBox(boxName);
-    await box.deleteAt(index);
-  }
-
-  Future<void> clearAll() async {
-    final box = await Hive.openBox(boxName);
-    await box.clear();
-  }
+Future<Database> openMyDatabase() async {
+  return openDatabase(
+    'my_database.db',
+    version: 1,
+    onCreate: (db, version) async {
+      await db.execute('''
+        CREATE TABLE products (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT,
+          brand TEXT,
+          category TEXT,
+          ingredients TEXT,
+          calories TEXT,
+          imageUrl TEXT,
+          type TEXT
+        )
+      ''');
+    },
+  );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import mobile_scanner
 import path_provider_foundation
 import photo_manager
 import shared_preferences_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
